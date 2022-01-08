2e95cf21834ddfff922f47e3e26c1dd7a0b76ea7
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-08 12:24:49 +0400

    feat; connecting to the database with mongoose

--- server.js
+++ server.js
@@ -28,4 +28,12 @@ app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+    PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
