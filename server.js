a1941bcd4aaab170a30f57f60f9e9f36c1c70f6e
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-08 10:58:47 +0400

    test

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+const req = require('express/lib/request');
+
 const app = express();
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+
+
+//Mount routers
+app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
