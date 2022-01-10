0c73da99dfa7a0e72690f8972b2adbcfbd2479ff
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-10 23:32:23 +0400

    feat: mongoose ErrorHandling [2]

--- middleware/error.js
+++ middleware/error.js
@@ -12,8 +12,18 @@ const errorHandler = (err, req, res, next) =>{
         const message = `Resource not found with id  of ${err.value}`;
         error = new ErrorResponse(message, 404);
     }
-    console.log(err.name);
-     
+
+    //Mongoose duplicate key
+    if (err.code === 11000) {
+        const message = 'Duplicate field value entered';
+        error = new ErrorResponse(message, 400);
+    }
+
+    //Mongoose validation error
+    if (err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(val => val.message);
+        error = new ErrorResponse(message, 400);
+    }
     res.status(error.statusCode || 500).json({
         success:false,
         error: error.message || 'Server Error'
