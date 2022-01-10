69b8660a6fc95f74cac2b6e96e8354d08db4d975
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-10 22:56:47 +0400

    feat: mongoose error handling[1]

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -11,7 +11,7 @@ exports.getBootcamps = async (req, res, next) => {
         res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps });
     }
     catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 }
 
@@ -32,7 +32,7 @@ exports.getBootcamp = async (req, res, next) => {
         res.status(200).json({ success: true, data: bootcamp });
     }
     catch (err) {
-        next(new ErrorResponse(`Bootcamp not found with id  of ${req.params.id}`, 404));
+        next(err);
     }
 }
 
@@ -49,7 +49,7 @@ exports.createBootcamp = async (req, res, next) => {
         });
     }
     catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 }
 
@@ -64,11 +64,13 @@ exports.updateBootcamp = async (req, res, next) => {
             runValidators: true
         });
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(
+                new ErrorResponse(`Bootcamp not found with id  of ${req.params.id}`, 404)
+            );
         }
         res.status(200).json({ success: true, data: bootcamp });
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 };
 
@@ -80,10 +82,12 @@ exports.deleteBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(
+                new ErrorResponse(`Bootcamp not found with id  of ${req.params.id}`, 404)
+            );
         }
         res.status(200).json({ success: true, data: {} });
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 }
--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,22 @@
-const errorHandler = (err, req, res, next) =>{
-    //Log to console for dev
-    console.log(err.stack);
+const ErrorResponse = require("../utils/errorResponse");
 
-    res.status(err.statusCode || 500).json({
+const errorHandler = (err, req, res, next) =>{
+    let error = {...err};
+
+    error.message = err.message;
+    //Log to console for dev
+    console.log(err);
+
+    //Mongoose bad ObjectId
+    if (err.name==='CastError') {
+        const message = `Resource not found with id  of ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    }
+    console.log(err.name);
+     
+    res.status(error.statusCode || 500).json({
         success:false,
-        error: err.message || 'Server Error'
+        error: error.message || 'Server Error'
     });
 }
 module.exports = errorHandler;
