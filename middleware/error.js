9e33e04762fc45fe23a90605efb18525719074b5
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-10 22:40:43 +0400

    feat: custom ErrorResponse class

--- middleware/error.js
+++ middleware/error.js
@@ -2,9 +2,9 @@ const errorHandler = (err, req, res, next) =>{
     //Log to console for dev
     console.log(err.stack);
 
-    res.status(500).json({
+    res.status(err.statusCode || 500).json({
         success:false,
-        error: err.message
+        error: err.message || 'Server Error'
     });
 }
 module.exports = errorHandler;
