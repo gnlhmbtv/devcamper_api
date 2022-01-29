005af53134bba25b6512264eaea62abf3f50aea5
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-29 17:21:11 +0400

    feat: cascade delete

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -126,12 +126,14 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 //@route     DELETE /api/v1/bootcamps/:id
 //@access    Private 
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+        const bootcamp = await Bootcamp.findById(req.params.id);
         if (!bootcamp) {
             return next(
                 new ErrorResponse(`Bootcamp not found with id  of ${req.params.id}`, 404)
             );
-        }
+        }   
+
+        bootcamp.remove();
         
         res.status(200).json({ success: true, data: {} });
 });
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -102,7 +102,7 @@ const BootcampSchema = new mongoose.Schema({
         default: Date.now
     }
 },{
-    toJson: {virtuals: true},
+    toJSON: {virtuals: true},
     toObject: {virtuals: true}
 });
 
@@ -128,6 +128,13 @@ BootcampSchema.pre('save', async function(next){
 
    //Do not save address in DB
    this.address = undefined;
+    next();
+});
+
+//Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+    console.log(`Courses being removed from bootcamp ${this._id}`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
     next();
 });
 
