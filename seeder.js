e90367e649522eda879a856112e80ff0344e49f1
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-01-30 20:35:29 +0400

    feat:aggregate-calculating the average course cost

--- seeder.js
+++ seeder.js
@@ -25,7 +25,7 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'u
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
-        await Course.create(courses);
+        //await Course.create(courses);
         console.log('Data imported...');
         process.exit();
     } catch (err) {
