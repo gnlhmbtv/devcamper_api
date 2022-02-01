d6fcefe50574f42bc413510f9c555faea1b721b0
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-02-02 00:35:15 +0400

    feat: user login

--- models/User.js
+++ models/User.js
@@ -45,7 +45,12 @@ UserSchema.pre('save', async function(next){
 UserSchema.methods.getSignedJwtToken = function(){
     return jwt.sign({ id: this._id}, process.env.JWT_SECRET,{
         expiresIn: process.env.JWT_EXPIRE
-    })
-}
+    });
+};
+
+//Match user entered password to hashed password in database
+UserSchema.methods.matchPassword =  async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
