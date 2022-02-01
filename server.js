291b756dc45b69e6c391aa100df925e20b117af0
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-02-01 19:59:36 +0400

    feat: user model

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 //Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
-//const req = require('express/lib/request');
+const auth = require('./routes/auth'); 
 
 const app = express();
 
@@ -37,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Mount routers
 app.use('/api/v1/bootcamps',bootcamps);
 app.use('/api/v1/courses',courses);
+app.use('/api/v1/auth',auth);
 
 app.use(errorHandler);
 
