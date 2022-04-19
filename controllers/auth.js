37a9d77b95aec744b39a8663150d7d52ca39c4f9
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-04-19 22:04:38 +0400

    feat: reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
@@ -51,7 +52,7 @@ exports.login = asyncHandler( async (req, res, next) => {
 
 
 
-//@desc      Get currend logged in user
+//@desc      Get current logged in user
 //@route     POST /api/v1/auth/me
 //@access    Private 
 exports.getMe = asyncHandler(async (req, res, next) => {
@@ -80,7 +81,7 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     await user.save({validateBeforeSave : false});
 
     // Create reset url
-    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
 
     const message = `You are receiving this email because you (or someone else) has requested the reset
     of a password. Please make a PUT request to: \n\n ${resetUrl}`;
@@ -109,7 +110,33 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 });
 
 
-// Get token from module, create cookie and send responce
+//@desc      Reset password
+//@route     PUT /api/v1/auth/resetpassword/:resettoken
+//@access    Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+    //Get hashed token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.resettoken).digest('hex');
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: {$gt: Date.now()}
+    });
+
+    if(!user){
+        return next(new ErrorResponse('Invalid token', 400));
+    }
+
+    //Set new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+});
+
+
+// Get token from module, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
 
     //Create token
