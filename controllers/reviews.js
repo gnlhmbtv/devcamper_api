a38f7353ad0a41a2a5dcc0948dd4731cc95d1b95
@WIN-DC6349VDB7B <guneljh@code.edu.az>
2022-04-23 10:40:04 +0400

    feat: get single review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -23,3 +23,21 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
         res.status(200).json(res.advancedResults);
     }
 });
+
+
+
+//@desc      Get single review
+//@route     GET /api/v1/reviews/:id
+//@access    Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+    const review = await Review.findById(req.params.id).populate({
+        path: 'bootcamp',
+        select: 'name description'
+    });
+
+    if(!review){
+        return next(new ErrorResponse(`No review found with the id of ${req.params.id}`,404));
+    }
+
+    res.status(200).json({success: true, data: review});
+});
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {
-    getReviews
+    getReviews,
+    getReview
 } = require('../controllers/reviews');
 
 const Review = require('../models/Review');
@@ -19,5 +20,5 @@ router
         select: 'name description'
     }) ,getReviews)
 
-
+router.route('/:id').get(getReview);
 module.exports = router;
